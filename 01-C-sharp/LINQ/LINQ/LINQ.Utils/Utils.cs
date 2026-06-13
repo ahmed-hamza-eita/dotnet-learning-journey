ec27b6a4173389f63e3375bed27273a2a631498d
aheit <aeita4@yahoo.com>
2026-06-13 06:15:04 +0300

    feat: add LINQ.Utils shared class library

---
+++ 01-C-sharp/LINQ/LINQ/LINQ.Utils/Utils.cs
@@ -0,0 +1,14 @@
+﻿namespace LINQ.Utils;
+
+public static class Utils
+{
+    public static void Print<T>(this IEnumerable<T> source, string title)
+    {
+        Console.WriteLine();
+        Console.WriteLine("┌───────────────────────────────────────────────────────┐");
+        Console.WriteLine($"│   {title.PadRight(52, ' ')}│");
+        Console.WriteLine("└───────────────────────────────────────────────────────┘");
+        foreach (var item in source)
+            Console.WriteLine(item);
+    }
+}
