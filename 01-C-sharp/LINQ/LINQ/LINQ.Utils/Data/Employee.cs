a53e0a4371f125036ab28ccb0f503222241bc207
aheit <aeita4@yahoo.com>
2026-06-14 06:09:34 +0300

    refactor: override ToString in Employee class

--- 01-C-sharp/LINQ/LINQ/LINQ.Utils/Data/Employee.cs
+++ 01-C-sharp/LINQ/LINQ/LINQ.Utils/Data/Employee.cs
@@ -1,21 +1,23 @@
- 
-    public class Employee
-    { 
-        public int Id { get; set; }
 
-        public string FirstName { get; set; }
+public class Employee
+{
+    public int Id { get; set; }
 
-        public string LastName { get; set; }
-         
-        public string Email { get; set; }
+    public string FirstName { get; set; }
 
-        public List<string> Skills { get; set; } = new List<string>();
+    public string LastName { get; set; }
+
+    public string Email { get; set; }
+
+    public List<string> Skills { get; set; } = new List<string>();
 
 
-        public string FullName => $"{FirstName} {LastName}";
+    public string FullName => $"{FirstName} {LastName}";
 
-         
+     public override string ToString()
+    {
+        return $"{Id} - {FirstName} {LastName} ({Email})";
     }
+}
 
-    
- 
+
