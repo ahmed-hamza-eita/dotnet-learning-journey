1c783c81132451daaf2877ed3b2a413bf5d473f2
aheit <aeita4@yahoo.com>
2026-06-16 18:01:35 +0300

    feat: data partitioning skip and take methods

---
+++ 01-C-sharp/LINQ/LINQ/Topics/04_Data_Partitioning/Program.cs
@@ -0,0 +1,21 @@
+﻿using LINQ.Utils;
+
+Console.WriteLine("Data Partitioning.................");
+
+var employees = Repository.LoadEmployees();
+
+#region Skip
+
+employees.Skip(2).Print("Skip First 2 elements");
+employees.SkipLast(2).Print("Skip Last 2 elements");
+employees.SkipWhile(emp => emp.Index != 2).Print("");
+
+#endregion
+
+
+#region Take
+employees.Take(2).Print("Take First 2 elements");
+employees.TakeLast(2).Print("Take Last 2 elements");
+employees.TakeWhile(emp => emp.Index != 2).Print("");
+#endregion
+
