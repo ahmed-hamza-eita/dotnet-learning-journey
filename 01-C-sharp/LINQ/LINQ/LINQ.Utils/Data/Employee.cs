588b2e95e88afb40cf065766473fcdc29d0aa6eb
aheit <aeita4@yahoo.com>
2026-06-16 17:44:26 +0300

    refactor: update shared models and repository

--- 01-C-sharp/LINQ/LINQ/LINQ.Utils/Data/Employee.cs
+++ 01-C-sharp/LINQ/LINQ/LINQ.Utils/Data/Employee.cs
@@ -1,23 +1,14 @@
-
 public class Employee
 {
-    public int Id { get; set; }
+    public int Index { get; set; }
+    public string EmployeeNo { get; set; }
 
-    public string FirstName { get; set; }
-
-    public string LastName { get; set; }
+    public string Name { get; set; }
 
     public string Email { get; set; }
+    public decimal Salary { get; set; }
 
     public List<string> Skills { get; set; } = new List<string>();
-
-
-    public string FullName => $"{FirstName} {LastName}";
-
-     public override string ToString()
-    {
-        return $"{Id} - {FirstName} {LastName} ({Email})";
-    }
 }
 
 
--- 01-C-sharp/LINQ/LINQ/LINQ.Utils/Data/Repository.cs
+++ 01-C-sharp/LINQ/LINQ/LINQ.Utils/Data/Repository.cs
@@ -2,42 +2,54 @@
 public static class Repository
 {
 
-    public static IEnumerable<Employee> LoadEmployees()
-    {
-        return new List<Employee>
+        public static IEnumerable<Employee> LoadEmployees()
+        {
+                return new List<Employee>
             {
-            new Employee
+                new Employee
                 {
-                        Id = 1001,
-                        FirstName = "Cochran",
-                        LastName = "Cole",
-                        Email = "Cole.Cochran@example.com",
-                        Skills = new() {"ASP.NET" , "CSS" , "Oracle"}
+                     Index = 1,
+                     EmployeeNo = "2017-FI-8516",
+                     Name = "Cochran Cole",
+                     Email = "Cole.Cochran@example.com",
+                     Salary = 103200,
+                     Skills = new() {"Javascript" , "NodeJS"  }
                 },
                 new Employee
                 {
-                        Id = 1002,
-                        FirstName = "Jaclyn",
-                        LastName = "Wolfe",
-                        Email = "Wolfe.Jaclyn@example.com",
-                        Skills = new() {"ASP.NET" , "SQL Server" , "Javascript" , "CSS" , "HTML"}
+                     Index = 2,
+                     EmployeeNo = "2018-FI-4815",
+                     Name = "Jaclyn Wolfe",
+                     Email = "Wolfe.Jaclyn@example.com",
+                     Salary = 192400,
+                     Skills = new() {"C++" , "Javascript" , "Oracle"  }
                 },
                 new Employee
                 {
-                        Id = 1003,
-                        FirstName = "Warner",
-                        LastName = "Jones",
-                        Email = "Jones.Warner@example.com",
-                        Skills = new() {"HTML" , "Oracle" , "SQL Server"}
+                     Index = 3,
+                     EmployeeNo = "2016-IT-1329",
+                     Name = "Warner Jones",
+                     Email = "Jones.Warner@example.com",
+                     Salary = 172800,
+                     Skills = new() {"NodeJS" , "C++"  }
                 },
                 new Employee
                 {
-                        Id = 1004,
-                        FirstName = "Hester",
-                        LastName = "Evans",
-                        Email = "Evans.Hester@example.com",
-                        Skills = new() {"ASP.NET"}
-                }
-            };
-    }
+                     Index = 4,
+                     EmployeeNo = "2016-FI-8336",
+                     Name = "Hester Evans",
+                     Email = "Evans.Hester@example.com",
+                     Salary = 155500,
+                     Skills = new() {"C++" , "SQL Server"  }
+                },
+                new Employee
+                {
+                     Index = 5,
+                     EmployeeNo = "2014-IT-3129",
+                     Name = "Wallace Buck",
+                     Email = "Buck.Wallace@example.com",
+                     Salary = 315800,
+                     Skills = new() {"HTML" , "ASP.NET" , "NodeJS"  }
+                }};
+        }
 }
