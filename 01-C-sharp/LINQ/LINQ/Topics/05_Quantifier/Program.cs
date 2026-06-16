ce90e628483814a3b661c8fc74724de1a668c46c
aheit <aeita4@yahoo.com>
2026-06-16 22:35:14 +0300

    feat: Quantifiers (Any, All and Contains)

---
+++ 01-C-sharp/LINQ/LINQ/Topics/05_Quantifier/Program.cs
@@ -0,0 +1,21 @@
+﻿using LINQ.Utils;
+Console.WriteLine("Quantifiers.....");
+
+var employees = Repository.LoadEmployees();
+
+// Any
+var anyResult = employees.Any(emp => emp.Name
+                .StartsWith("jac", StringComparison.OrdinalIgnoreCase));
+Console.WriteLine($"Any Result: {anyResult}");
+
+// All
+var allResult1 = employees.All
+                (empEmail => !string.IsNullOrWhiteSpace(empEmail.Email));
+
+var allResult2 = employees.All(empSkills => empSkills.Skills.Any(s => s == "c#"));
+
+Console.WriteLine($"All Result 1: {allResult1} || All Result 2: {allResult2}");
+
+// Contains
+var containsResult = employees.Any(emp => emp.Name.Contains("co",StringComparison.OrdinalIgnoreCase));
+Console.WriteLine($"Contains Result: {containsResult}");
