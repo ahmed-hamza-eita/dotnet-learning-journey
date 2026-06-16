be70351de27aa48a0e12fb94563bf264b3b29ea7
aheit <aeita4@yahoo.com>
2026-06-16 18:47:41 +0300

    feat: data partitioning chunk method

--- 01-C-sharp/LINQ/LINQ/Topics/04_Data_Partitioning/Program.cs
+++ 01-C-sharp/LINQ/LINQ/Topics/04_Data_Partitioning/Program.cs
@@ -1,4 +1,5 @@
-﻿using LINQ.Utils;
+﻿using System.Text.RegularExpressions;
+using LINQ.Utils;
 
 Console.WriteLine("Data Partitioning.................");
 
@@ -19,3 +20,10 @@ employees.TakeLast(2).Print("Take Last 2 elements");
 employees.TakeWhile(emp => emp.Index != 2).Print("");
 #endregion
 
+#region  Chunk
+employees.Chunk(2) //Splits the collection into smaller groups .
+.Select((chunk, index) => new { Group = chunk, Index = index }) // chunk → the current group, index → the group's position (starting from 0).
+.ToList() // Converts the result from an IEnumerable<T> into a List<T>.
+.ForEach(c => c.Group.Print($"Group {c.Index + 1}")); // is available on List<T> but not on IEnumerable<T>.
+ 
+#endregion
