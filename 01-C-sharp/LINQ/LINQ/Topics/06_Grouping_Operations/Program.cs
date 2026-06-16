7d75370696e5c4cea1f43cfd613c6cf1229185b3
aheit <aeita4@yahoo.com>
2026-06-17 00:23:02 +0300

    feat: grouping operations GroupBy and ToLookup

---
+++ 01-C-sharp/LINQ/LINQ/Topics/06_Grouping_Operations/Program.cs
@@ -0,0 +1,21 @@
+﻿using LINQ.Utils;
+
+Console.WriteLine(" Grouping Operations.....");
+
+var employees = Repository.LoadEmployees();
+
+
+//Group By (Deferred (Lazy) Excution)
+var groupedEmployees = employees.GroupBy(emp => emp.Department);
+foreach (var group in groupedEmployees)
+{
+    group.Print($"Employees in {group.Key} With GroupBy ");
+}
+
+
+//ToLookup (Immediate Excution)
+var lookupEmployees = employees.ToLookup(emp => emp.Department);
+foreach (var lookup in lookupEmployees)
+{
+    lookup.Print($"Employees in {lookup.Key} With Lookup");
+}
