137290d713d1143eab63193033546bfed7bfb842
aheit <aeita4@yahoo.com>
2026-06-17 03:44:28 +0300

    feat: join data with (Join and GroupJoin)

---
+++ 01-C-sharp/LINQ/LINQ/Topics/07_join/Group.cs
@@ -0,0 +1,6 @@
+class Group
+{
+    public string Department { set; get; }
+    public List<string> Employees { set; get; }
+
+}
---
+++ 01-C-sharp/LINQ/LINQ/Topics/07_join/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.RegularExpressions;
+using LINQ.Utils;
+Console.WriteLine("Join.......");
+
+var employees = Repository.LoadEmployees();
+var dept = Repository.LoadDepartment();
+
+//Join
+var joinResult = employees.Join(
+    dept,
+    emp => emp.DepartmentId,
+    dept => dept.Id,
+    (emp, dept) => new EmployeeDto
+    {
+        FullName = emp.FullName,
+        Department = dept.Name
+    }
+);
+foreach (var item in joinResult)
+{
+    Console.WriteLine($"{item.FullName} [{item.Department}]");
+}
+
+//Group join
+
+var groupJoin = dept.GroupJoin(
+                                employees,
+                                dept => dept.Id,
+                                emps => emps.DepartmentId,
+                                (dept, emps) => new Group
+                                {
+                                    Department = dept.Name,
+                                    Employees = emps.Select(e => e.FullName).ToList()
+                                }
+                              );
+
