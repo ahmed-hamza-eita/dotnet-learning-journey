aa693e857d03d4cbdf8274a570384615f2e75cd8
aheit <aeita4@yahoo.com>
2026-06-17 23:38:34 +0300

    feat: equality operations (SequenceEqual)

---
+++ 01-C-sharp/LINQ/LINQ/Topics/10_equality_operations/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine(" Equality Operations......");
+
+
+
+/*SequenceEqual -> It returns true if both sequences contain
+                the exact same number of elements,
+                the same values,
+                and in the exact same order 
+                otherwise, it returns false
+                 */
+var list1 = new List<int>() { 1, 2, 3, 4 };
+var list2 = new List<int>() { 1, 2, 4, 5 };
+
+var sequenceEqual = list1.SequenceEqual(list2);
+Console.WriteLine($"List 1 and List 2  {(sequenceEqual ? "are" : "are not")} Equal");
+
+
+
+
