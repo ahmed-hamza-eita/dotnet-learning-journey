6c0b02c18f194f9339dd94aeb502d37384ba0a51
aheit <aeita4@yahoo.com>
2026-06-17 18:51:33 +0300

    feat: generate operations (range, repeat, and empty)

---
+++ 01-C-sharp/LINQ/LINQ/Topics/08_generate_operations/Program.cs
@@ -0,0 +1,12 @@
+﻿using LINQ.Utils;
+Console.WriteLine("Generate Operations..........");
+
+//Range 
+Enumerable.Range(1,10).Print("Range");
+
+//Reapet
+Enumerable.Repeat("Ahmed",3).Print("Repeat");
+
+// Empty Sequence
+Enumerable.Empty<int>().Print("Empty Sequence");
+
