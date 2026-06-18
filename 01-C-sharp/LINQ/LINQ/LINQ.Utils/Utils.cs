109267018a5aae8df24ef1d45e3d5241b2a8ac0d
aheit <aeita4@yahoo.com>
2026-06-18 15:57:16 +0300

    refactor: print method

--- 01-C-sharp/LINQ/LINQ/LINQ.Utils/Utils.cs
+++ 01-C-sharp/LINQ/LINQ/LINQ.Utils/Utils.cs
@@ -4,11 +4,19 @@ public static class Utils
 {
     public static void Print<T>(this IEnumerable<T> source, string title)
     {
-        Console.WriteLine();
-        Console.WriteLine("┌───────────────────────────────────────────────────────┐");
+        var defaultColor = Console.ForegroundColor;
+        
+        Console.ForegroundColor = ConsoleColor.DarkGreen;
+        Console.WriteLine("\n┌───────────────────────────────────────────────────────┐");
         Console.WriteLine($"│   {title.PadRight(52, ' ')}│");
         Console.WriteLine("└───────────────────────────────────────────────────────┘");
+        Console.ForegroundColor = defaultColor;
+
         foreach (var item in source)
+        {
             Console.WriteLine(item);
+        }
     }
+
+    
 }
