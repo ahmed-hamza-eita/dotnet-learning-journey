ab58016d90c223ba3bb2823d5d654382532ff0fb
aheit <aeita4@yahoo.com>
2026-06-18 05:57:49 +0300

    feat: concat and union)

---
+++ 01-C-sharp/LINQ/LINQ/Topics/11_concat/Program.cs
@@ -0,0 +1,22 @@
+﻿using LINQ.Utils;
+Console.WriteLine("Concat......");
+
+//concat
+var currentEmp = new List<string> { "Ahmed", "Sara", "Ahmed" };
+var newHires = new List<string> { "Ali", "Mona" };
+var allEmp = currentEmp.Concat(newHires); //Don,t remove duplicate
+foreach (var i in allEmp)
+{
+    Console.Write($"{i}  ");
+
+}
+
+Console.WriteLine("");
+
+//Union
+var allEmp2 = currentEmp.Union(newHires); //remove duplicate
+foreach (var i in allEmp2)
+{
+    Console.Write($"{i}  ");
+
+}
