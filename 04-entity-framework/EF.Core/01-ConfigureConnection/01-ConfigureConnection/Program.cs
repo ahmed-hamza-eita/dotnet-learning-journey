0e0fca3994ac2c3df4685d5f9912e41b2cb183fa
aheit <aeita4@yahoo.com>
2026-06-25 03:54:57 +0300

    feat: add EF Core project with connection string configuration

---
+++ 04-entity-framework/EF.Core/01-ConfigureConnection/01-ConfigureConnection/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace _01_ConfigureConnection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .Build();
+            Console.WriteLine(configuration.GetSection("DefaultConnection").Value);
+        }
+    }
+}
+ 
