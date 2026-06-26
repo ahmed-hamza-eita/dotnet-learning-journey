4e169e1512c06affacd6513cf272e7fbcfb5dae9
aheit <aeita4@yahoo.com>
2026-06-26 03:45:00 +0300

    feat: Execute Transaction

---
+++ 04-entity-framework/EF.Core/08-ExecuteTransaction/Program.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Linq.Expressions;
+using System.Transactions;
+
+namespace _08_ExecuteTransaction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             var configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+            SqlConnection sqlConnection = new SqlConnection(configuration.GetSection("DefaultConnection").Value);
+
+            SqlCommand sqlCommand = sqlConnection.CreateCommand();
+
+            sqlCommand.CommandType = CommandType.Text;
+
+            sqlConnection.Open();
+
+            SqlTransaction sqlTransaction = sqlConnection.BeginTransaction();
+            sqlCommand.Transaction = sqlTransaction;
+
+            try
+            {
+                sqlCommand.CommandText = "UPDATE Wallets Set Balance = Balance - 1000 Where Id = 2";
+                sqlCommand.ExecuteNonQuery();
+
+
+                sqlCommand.CommandText = "UPDATE Wallets Set Balance = Balance + 1000 Where Id = 3";
+                sqlCommand.ExecuteNonQuery();
+
+                sqlTransaction.Commit();
+
+                Console.WriteLine("Transaction of transfer completed successfully");
+
+            }
+            catch
+            {
+                try
+                {
+                    sqlTransaction.Rollback();
+                }
+                catch
+                {
+                    // log errors
+                }
+            }
+            finally
+            {
+
+                try
+                {
+                    sqlConnection.Close();
+                }
+                catch
+                {
+                    // log errors
+
+                }
+            }
+
+
+        }
+    }
+}
