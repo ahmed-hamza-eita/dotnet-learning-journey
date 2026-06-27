10d5a601d97427bcffa9231eec376749b91fb1de
aheit <aeita4@yahoo.com>
2026-06-27 21:07:40 +0300

    feat: retrieve data

--- 04-entity-framework/EF.Core/NHibernate Overview/NHibernate/Nhibernate Overview/Program.cs
+++ 04-entity-framework/EF.Core/NHibernate Overview/NHibernate/Nhibernate Overview/Program.cs
@@ -1,12 +1,30 @@
-﻿using Shared.Session;
+﻿using NHibernate;
+using Shared.Models;
+using Shared.Session;
 namespace Nhibernate_Overview
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            using(  var session = CreateSession._CreateSession()) {
-                Console.WriteLine(session.IsConnected);
+            using (var session = CreateSession._CreateSession())
+            {
+                Console.WriteLine($"Conntected: {session.IsConnected}");
+                using (var transaction = session.BeginTransaction())
+                {
+                    //excute select statement
+                    retrieveData(session);
+                }
+            }
+        }
+
+
+        public static void retrieveData(ISession session) {
+
+            var getWalletsData = session.Query<Wallet>();
+
+            foreach (var w in getWalletsData) {
+                Console.WriteLine(w);
             }
         }
     }
