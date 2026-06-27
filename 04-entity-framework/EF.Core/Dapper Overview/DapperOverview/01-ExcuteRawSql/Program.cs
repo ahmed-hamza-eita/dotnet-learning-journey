7787957c1bf6930592e44bb4e4f507fd1973c3dc
aheit <aeita4@yahoo.com>
2026-06-27 08:55:08 +0300

    feat: insert statement

--- 04-entity-framework/EF.Core/Dapper Overview/DapperOverview/01-ExcuteRawSql/Program.cs
+++ 04-entity-framework/EF.Core/Dapper Overview/DapperOverview/01-ExcuteRawSql/Program.cs
@@ -15,15 +15,22 @@ namespace _01_ExcuteRawSql
                 var sqlConnection = DatabaseConfig.CreateConnection();
 
                 //Excute Raw Sql
-                rawSql(sqlConnection);
+                // rawSql(sqlConnection);
 
-               
+                //Insert Statement
+                //insertStatement(sqlConnection, new Wallet {Holder = "Waleed" , Balance = 45000m });
+
+                //Insert Statement with id
+                // insertStatementReturnedId(sqlConnection, new Wallet {Holder = "hamo" , Balance = 450000m });
+
+
 
             }
 
         }
 
-        public static void rawSql(SqlConnection sqlConnection) {
+        public static void rawSql(SqlConnection sqlConnection)
+        {
             //select query
             var sqlQuery = "SELECT * FROM WALLETS";
             //excute query
@@ -33,6 +40,34 @@ namespace _01_ExcuteRawSql
             {
                 Console.WriteLine(item);
             }
+        }
+        public static void insertStatement(SqlConnection sqlConnection, Wallet wallet)
+        {
+            var sqlQuery = "INSERT INTO Wallets (Holder,Balance) " +
+                "VALUES (@Holder , @Balance)";
+
+            sqlConnection.Execute(sqlQuery, new
+            {
+                Holder = wallet.Holder,
+                Balance = wallet.Balance
+            });
+        }
+
+
+        public static void insertStatementReturnedId(SqlConnection sqlConnection, Wallet wallet)
+        {
+            var sqlQuery = "INSERT INTO Wallets (Holder,Balance) " +
+                "VALUES (@Holder , @Balance)" +
+                "SELECT CAST(SCOPE_IDENTITY() AS INT)";
+
+            var parmeters = new
+            {
+                Holder = wallet.Holder,
+                Balance = wallet.Balance
+            };
+
+            wallet.Id = sqlConnection.Query<int>(sqlQuery, parmeters).Single();
+            Console.WriteLine(wallet);
         }
 
     }
