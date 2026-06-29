bb8af42243a7ac0488ec0430dd9a852337f97283
aheit <aeita4@yahoo.com>
2026-06-29 10:07:44 +0300

    feat: init EFCore and configuration DB Connection

---
+++ 04-entity-framework/EFCore/Shared/DBConfiguration/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared.DBConfiguration
+{
+    internal class AppDbContext
+    {
+    }
+}
---
+++ 04-entity-framework/EFCore/Shared/Models/Wallet.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace Shared.Models
+{
+    public class Wallet
+    {
+        public int Id { set; get; }
+        public string? Holder { set; get; }
+        public decimal? Balance { set; get; }
+
+        public override string ToString()
+        {
+            return $"[{Id}] {Holder} {Balance:C}";
+        }
+    }
+}
