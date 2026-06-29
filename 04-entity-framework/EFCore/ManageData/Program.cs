8a5474c1c3351ebae4a7d68857b95f0c0decd951
aheit <aeita4@yahoo.com>
2026-06-29 11:38:37 +0300

    feat: transaction

--- 04-entity-framework/EFCore/ManageData/Program.cs
+++ 04-entity-framework/EFCore/ManageData/Program.cs
@@ -23,7 +23,11 @@ namespace ManageData
                 //Delete
                 //deleteData(context, 1005);
 
-                queryData(context);
+                //queryData(context);
+
+                //transaction
+                transaction(context,1002,1003);
+                getData(context);
             }
 
         }
@@ -66,11 +70,33 @@ namespace ManageData
             context.SaveChanges();
         }
 
-        public static void queryData(AppDbContext context) {
-            var result = context.Wallets.Where(x=>x.Balance >=500000);
+        public static void queryData(AppDbContext context)
+        {
+            var result = context.Wallets.Where(x => x.Balance >= 500000);
 
-            foreach (var i in result) {
+            foreach (var i in result)
+            {
                 Console.WriteLine(i);
+            }
+        }
+
+        public static void transaction(AppDbContext context, int fromId, int toId)
+        {
+
+            using (var t = context.Database.BeginTransaction())
+            {
+                //transfer 10000 from id=5 to id = 6
+                var amount = 1000m;
+                var _fromId = context.Wallets.Single(x => x.Id == fromId);
+                var _toId = context.Wallets.Single(x => x.Id == toId);
+
+                _fromId.Balance -= amount;
+                context.SaveChanges();
+
+                _toId.Balance += amount;
+                context.SaveChanges();
+
+                t.Commit();
             }
         }
     }
