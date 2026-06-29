3856510e891d7ad0db79e999c29b2dfec825c6c7
aheit <aeita4@yahoo.com>
2026-06-29 10:34:53 +0300

    feat: select statement and edit json file

---
+++ 04-entity-framework/EFCore/ManageData/Program.cs
@@ -0,0 +1,27 @@
+﻿using Shared.DBConfiguration;
+
+namespace ManageData
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new AppDbContext())
+            {
+                //Retrieve data (select statement)
+                getData(context);
+
+            }
+
+        }
+
+        public static void getData(AppDbContext context)
+        {
+
+            foreach (var wallet in context.Wallets)
+            {
+                Console.WriteLine(wallet);
+            }
+        }
+    }
+}
--- 04-entity-framework/EFCore/Shared/DBConfiguration/AppDbContext.cs
+++ 04-entity-framework/EFCore/Shared/DBConfiguration/AppDbContext.cs
@@ -1,10 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Shared.Models;
 
 namespace Shared.DBConfiguration
 {
-    internal class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public DbSet<Wallet> Wallets { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            var configuration = new ConfigurationBuilder().
+                AddJsonFile("appsettings.json").Build();
+            var connectionStr = configuration.GetSection("DefaultConnection").Value;
+
+            optionsBuilder.UseSqlServer(connectionStr);
+        }
     }
 }
