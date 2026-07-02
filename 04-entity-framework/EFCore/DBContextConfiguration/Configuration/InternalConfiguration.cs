18a88a12fdd4b13a30a6c2bfbfcf872c6b402a7c
aheit <aeita4@yahoo.com>
2026-07-02 07:19:17 +0300

    feat: Internal Configuration

---
+++ 04-entity-framework/EFCore/DBContextConfiguration/Configuration/InternalConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DBContextConfiguration.Configuration
+{
+    public class InternalConfiguration:DbContext
+    {
+        public DbSet<Wallet> wallets { set; get; } = null!;
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            var connectionString = configuration.GetSection("DefaultConnection").Value;
+
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
--- 04-entity-framework/EFCore/DBContextConfiguration/Program.cs
+++ 04-entity-framework/EFCore/DBContextConfiguration/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace DBContextConfiguration
+﻿using DBContextConfiguration.Configuration;
+using Shared;
+using Shared.Models;
+namespace DBContextConfiguration
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //InternalConfiguration
+            InternalConfiguration();
+        }
+
+        public static void InternalConfiguration()
+        {
+            using (var context = new InternalConfiguration())
+            {
+                foreach (var w in context.wallets)
+                {
+
+                    Console.WriteLine(w);
+                }
+            }
         }
     }
 }
