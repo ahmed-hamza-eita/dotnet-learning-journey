fef1bca29d4f967bb0743a908ac862d4561b525f
aheit <aeita4@yahoo.com>
2026-07-04 06:16:45 +0300

    feat: init EFCore Configuration

---
+++ 04-entity-framework/EFCore/07-EFCoreConfiguration/07-EFCoreConfiguration/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _07_EFCoreConfiguration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
