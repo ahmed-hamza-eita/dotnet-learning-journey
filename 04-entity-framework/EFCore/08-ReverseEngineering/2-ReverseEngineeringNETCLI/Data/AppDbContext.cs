384eaba918ac966986aa2e9fe1fff6c6c66d3923
aheit <aeita4@yahoo.com>
2026-07-04 23:05:31 +0300

    feat: reverse Engineering by .NET CLI

---
+++ 04-entity-framework/EFCore/08-ReverseEngineering/2-ReverseEngineeringNETCLI/Data/AppDbContext.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using _2_ReverseEngineeringNETCLI.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace _2_ReverseEngineeringNETCLI.Data;
+
+public partial class AppDbContext : DbContext
+{
+    public AppDbContext()
+    {
+    }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Event> Events { get; set; }
+
+    public virtual DbSet<Speaker> Speakers { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=.; initial catalog = TechTalk ; Integrated Security = SSPI; TrustServerCertificate=TRUE");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Event>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Events__3214EC0723FE2C3A");
+
+            entity.Property(e => e.EndAt).HasColumnType("datetime");
+            entity.Property(e => e.StartAt).HasColumnType("datetime");
+            entity.Property(e => e.Title)
+                .IsRequired()
+                .HasMaxLength(255);
+
+            entity.HasOne(d => d.Speaker).WithMany(p => p.Events)
+                .HasForeignKey(d => d.SpeakerId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Events__SpeakerI__4CA06362");
+        });
+
+        modelBuilder.Entity<Speaker>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Speakers__3214EC07429B53AC");
+
+            entity.Property(e => e.FirstName)
+                .IsRequired()
+                .HasMaxLength(25);
+            entity.Property(e => e.LastName)
+                .IsRequired()
+                .HasMaxLength(25);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ 04-entity-framework/EFCore/08-ReverseEngineering/2-ReverseEngineeringNETCLI/Program.cs
@@ -0,0 +1,19 @@
+﻿using _2_ReverseEngineeringNETCLI.Data;
+using System;
+
+namespace _2_ReverseEngineeringNETCLI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new AppDbContext())
+            {
+                foreach (var i in context.Speakers)
+                {
+                    Console.WriteLine(i.FirstName + i.LastName);
+                }
+            }
+        }
+    }
+}
