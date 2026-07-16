65e9abbc4c48da5c3216d79ba63d1b918d8375a1
aheit <aeita4@yahoo.com>
2026-07-16 12:20:56 +0300

    feat save data: Basic save

--- 04-entity-framework/EFCore/18_SaveData/18_SaveData/Program.cs
+++ 04-entity-framework/EFCore/18_SaveData/18_SaveData/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace _18_SaveData
+﻿using _18_SaveData.Data;
+using _18_SaveData.Entities;
+using _18_SaveData.Topics;
+using _18_SaveData.Utils;
+
+namespace _18_SaveData
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            using (var context = new AppDbContext())
+            {
+                #region Basic Save
+                BasicSave.RunBasicSave(context);
+                BasicSave.RunBasicUpdate(context);
+                BasicSave.RunBasicDelete(context);
+                BasicSave.RunMultipleOperationsWithSingleSave(context);
+                BasicSave.RunAddRelatedEntities(context);
+                #endregion
+
+
+            }
         }
+
+        
     }
 }
---
+++ 04-entity-framework/EFCore/18_SaveData/18_SaveData/Topics/1_BasicSave.cs
@@ -0,0 +1,51 @@
+﻿using _18_SaveData.Data;
+using _18_SaveData.Entities;
+using _18_SaveData.Utils;
+
+namespace _18_SaveData.Topics
+{
+    public static class  BasicSave
+    {
+        public static void RunBasicSave(AppDbContext context)
+        {
+            DbHelper.RecreateCleanDB();
+            var newAuthor = new Author { Id = 1, FName = "ahmed", LName = "Hamza" };
+            context.Authors.Add(newAuthor);
+            context.SaveChanges();
+        }
+
+        public static void RunBasicUpdate(AppDbContext context)
+        {
+            var updateAuthor = context.Authors.FirstOrDefault(x => x.Id == 1);
+            updateAuthor.FName = "AHMED";
+            context.SaveChanges();
+        }
+        public static void RunBasicDelete(AppDbContext context)
+        {
+            var deleteAuthor = context.Authors.FirstOrDefault(x => x.Id == 1);
+            context.Remove(deleteAuthor);
+            context.SaveChanges();
+        }
+        public static void RunMultipleOperationsWithSingleSave(AppDbContext context)
+        {
+            var au1 = new Author { Id = 1, FName = "Ahmed", LName = "Hamza" };
+            context.Authors.Add(au1);
+            var au2 = new Author { Id = 2, FName = "Walid", LName = "Mohamed" };
+            context.Authors.Add(au2);
+            var au3 = new Author { Id = 3, FName = "Hakim", LName = "Ali" };
+            context.Authors.Add(au3);
+
+            context.SaveChanges();
+        }
+       public static void RunAddRelatedEntities(AppDbContext context)
+        {
+            var author1 = context.Authors.FirstOrDefault(x => x.Id == 1);
+            author1.Books.Add(new Book
+            {
+                Id = 1,
+                Title = "New Book"
+            });
+            context.SaveChanges();
+        }
+    }
+}
