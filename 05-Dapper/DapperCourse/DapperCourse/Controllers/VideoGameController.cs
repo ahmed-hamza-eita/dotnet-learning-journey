a7f0136601637bd7e4e4c872b4889b07d54f234f
aheit <aeita4@yahoo.com>
2026-07-17 23:50:43 +0300

    feat Dapper: add controller

---
+++ 05-Dapper/DapperCourse/DapperCourse/Controllers/VideoGameController.cs
@@ -0,0 +1,18 @@
+﻿using DapperCourse.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DapperCourse.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VideoGameController : ControllerBase
+    {
+        private readonly IVideoGameRepository _videoGameRepository;
+
+        public VideoGameController(IVideoGameRepository videoGameRepository)
+        {
+            _videoGameRepository = videoGameRepository;
+        }
+    }
+}
