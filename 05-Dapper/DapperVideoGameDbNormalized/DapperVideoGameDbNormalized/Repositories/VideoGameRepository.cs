5dfd4b0fa59c3df79741a1bfee58654d9a5009b9
aheit <aeita4@yahoo.com>
2026-07-19 19:03:00 +0300

    feat DapperExample: enhance code

--- 05-Dapper/DapperVideoGameDbNormalized/DapperVideoGameDbNormalized/Repositories/VideoGameRepository.cs
+++ 05-Dapper/DapperVideoGameDbNormalized/DapperVideoGameDbNormalized/Repositories/VideoGameRepository.cs
@@ -26,9 +26,28 @@ namespace DapperVideoGameDbNormalized.Repositories
 
         public async Task<IEnumerable<VideoGame>> GetAllVideoGamesAsync()
         {
-            using (var connection = GetConnection())
-            {
-                var sql = @"
+            var sql = GetVideoGameSql(false);
+            var videoGame = await QueryVideoGameAsync(sql);
+            return videoGame;
+
+        }
+
+
+        public async Task<VideoGame> GetVideoGamesAsync(int Id)
+        {
+            var sql = GetVideoGameSql(true);
+            var videoGame = await QueryVideoGameAsync(sql, new { Id = Id });
+            return videoGame.FirstOrDefault();
+        }
+
+        public Task UpdateVideoGameAsync(VideoGame videoGame)
+        {
+            throw new NotImplementedException();
+        }
+
+        private string GetVideoGameSql(bool withWhereClause)
+        {
+            var sql = @"
                     SELECT vg.*, p.*, d.*, gd.*, r.*, pf.*
                     FROM VideoGames vg
                     LEFT JOIN Publishers p ON vg.PublisherId = p.Id
@@ -37,58 +56,19 @@ namespace DapperVideoGameDbNormalized.Repositories
                     LEFT JOIN Reviews r ON vg.Id = r.VideoGameId
                     LEFT JOIN VideoGamesPlatforms vgp ON vg.Id = vgp.VideoGameId
                     LEFT JOIN Platforms pf ON pf.Id = vgp.PlatformId";
-
-                var videoGameDict = new Dictionary<int, VideoGame>();
-
-                await connection.QueryAsync<VideoGame, Publisher, Developer, GameDetail, Review, Platform, VideoGame>
-                   (sql, (videoGame, publisher, developer, gameDetail, review, platform) =>
-                   {
-                       if (!videoGameDict.TryGetValue(videoGame.Id, out var currentGame))
-                       {
-                           currentGame = videoGame;
-                           currentGame.Publisher = publisher;
-                           currentGame.Developer = developer;
-                           currentGame.GameDetail = gameDetail;
-                           currentGame.Reviews = [];
-                           currentGame.Platforms = [];
-                           videoGameDict.Add(currentGame.Id, currentGame);
-
-                       }
-                       if (review is not null && !currentGame.Reviews.Any(r => r.Id == review.Id))
-                       {
-                           currentGame.Reviews.Add(review);
-                       }
-                       if (platform is not null && !currentGame.Platforms.Any(r => r.Id == platform.Id))
-                       {
-                           currentGame.Platforms.Add(platform);
-                       }
-                       return currentGame;
-                   }, splitOn: "Id ,Id,VideoGameId,Id,Id");
-                return videoGameDict.Values;
-            }
-
+            if (withWhereClause)
+                sql += " Where vg.Id = @Id";
+            return sql;
         }
-
-
-        public async Task<VideoGame> GetVideoGamesAsync(int Id)
+        private async Task<IEnumerable<VideoGame>> QueryVideoGameAsync(string Sql, object? Parameters = null)
         {
             using (var connection = GetConnection())
             {
-                var sql = @"
-                    SELECT vg.*, p.*, d.*, gd.*, r.*, pf.*
-                    FROM VideoGames vg
-                    LEFT JOIN Publishers p ON vg.PublisherId = p.Id
-                    LEFT JOIN Developers d ON vg.DeveloperId = d.Id
-                    LEFT JOIN GameDetails gd ON vg.Id = gd.VideoGameId
-                    LEFT JOIN Reviews r ON vg.Id = r.VideoGameId
-                    LEFT JOIN VideoGamesPlatforms vgp ON vg.Id = vgp.VideoGameId
-                    LEFT JOIN Platforms pf ON pf.Id = vgp.PlatformId
-                    Where vg.Id = @Id";
 
                 var videoGameDict = new Dictionary<int, VideoGame>();
 
                 await connection.QueryAsync<VideoGame, Publisher, Developer, GameDetail, Review, Platform, VideoGame>
-                   (sql, (videoGame, publisher, developer, gameDetail, review, platform) =>
+                   (Sql, (videoGame, publisher, developer, gameDetail, review, platform) =>
                    {
                        if (!videoGameDict.TryGetValue(videoGame.Id, out var currentGame))
                        {
@@ -111,17 +91,12 @@ namespace DapperVideoGameDbNormalized.Repositories
                        }
                        return currentGame;
                    }
-                   , new { Id = Id }
+                   , Parameters
                    , splitOn: "Id ,Id,VideoGameId,Id,Id");
-                return videoGameDict.Values.FirstOrDefault();
+                return videoGameDict.Values;
             }
-        }
 
-        public Task UpdateVideoGameAsync(VideoGame videoGame)
-        {
-            throw new NotImplementedException();
         }
-
         private SqlConnection GetConnection()
         {
             return new SqlConnection(_configuration.GetConnectionString("DefaultConnection"));
