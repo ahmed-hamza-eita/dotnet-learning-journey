275476c9235312a68fe897c4c4a11760f65b22c5
aheit <aeita4@yahoo.com>
2026-07-20 18:27:40 +0300

    feat DapperOverall: add entities

---
+++ 05-Dapper/DapperOvervall/DapperAspNetCore/DapperAspNetCore/Entities/Company.cs
@@ -0,0 +1,12 @@
+﻿namespace DapperAspNetCore.Entities
+{
+    public class Company
+    {
+        public int Id { set; get; }
+        public string Name { set; get; }
+        public string Address { set; get; }
+        public string Country { set; get; }
+
+        public List<Employee> Employees { set; get; } = new();
+    }
+}
---
+++ 05-Dapper/DapperOvervall/DapperAspNetCore/DapperAspNetCore/Entities/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace DapperAspNetCore.Entities
+{
+    public class Employee
+    {
+        public int Id { set; get; }
+        public string Name { set; get; }
+        public int Age { set; get; }
+        public string Position { set; get; }
+
+        public Company Company { set; get; }
+        public int CompanyId { set; get; }
+    }
+}
