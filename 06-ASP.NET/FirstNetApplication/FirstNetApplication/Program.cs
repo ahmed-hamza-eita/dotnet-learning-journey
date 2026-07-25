24a373454958a21e77ff45a8dee2b6fa553343c7
aheit <aeita4@yahoo.com>
2026-07-25 18:46:48 +0300

    feat(status code)

--- 06-ASP.NET/FirstNetApplication/FirstNetApplication/Program.cs
+++ 06-ASP.NET/FirstNetApplication/FirstNetApplication/Program.cs
@@ -1,6 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", async (HttpContext httpContext) =>
+{
+    if (httpContext.Request.Method == "GETt")
+    {
+        httpContext.Response.StatusCode = 200;
+        await httpContext.Response.WriteAsync("My Response");
+    }
+    else {
+        httpContext.Response.StatusCode = 401;
+        await httpContext.Response.WriteAsync("Error");
+    }
+    
+});
 
 app.Run();
