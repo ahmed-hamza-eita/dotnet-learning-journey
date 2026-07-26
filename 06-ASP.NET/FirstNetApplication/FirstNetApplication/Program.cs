684691a36f8853ed4ff4926a9d02226e487e53a0
aheit <aeita4@yahoo.com>
2026-07-26 19:37:21 +0300

    feat(Middleware): implement by delegate

--- 06-ASP.NET/FirstNetApplication/FirstNetApplication/Program.cs
+++ 06-ASP.NET/FirstNetApplication/FirstNetApplication/Program.cs
@@ -32,6 +32,7 @@ app.MapGet("/", async (HttpContext httpContext) =>
 });
 */
 
+/*
 //Post Method
 app.MapPost("/employees/add-employee", async (HttpContext httpContext) =>
 {
@@ -42,6 +43,33 @@ app.MapPost("/employees/add-employee", async (HttpContext httpContext) =>
         await httpContext.Response.WriteAsync($"Name is {response?.name} Age is {response?.age}");
     }
 
+});
+*/
+
+
+
+//MiddleWare
+app.Use(async (httpContext, next) =>
+{
+    await httpContext.Response.WriteAsync("First MiddleWare \t");
+    await next(httpContext); //call next middleware
+
+    await httpContext.Response.WriteAsync("\n After Mid 1");
+});
+
+app.Use(async (httpContext, next) =>
+{
+    await httpContext.Response.WriteAsync("Second MiddleWare \t");
+    await next(httpContext); //call next middleware
+
+    await httpContext.Response.WriteAsync("\n After Mid 2");
+});
+
+app.Use(async (HttpContext httpContext, RequestDelegate next) =>
+{
+    await httpContext.Response.WriteAsync("Third MiddleWare");
+
+    await httpContext.Response.WriteAsync("\n After Mid 3");
 });
 app.Run();
 
