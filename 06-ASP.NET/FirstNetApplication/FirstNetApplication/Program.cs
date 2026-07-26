537b5bb089b11faaf8e03b95d820951f670fca74
aheit <aeita4@yahoo.com>
2026-07-26 20:01:35 +0300

    feat(Middleware): task

--- 06-ASP.NET/FirstNetApplication/FirstNetApplication/Program.cs
+++ 06-ASP.NET/FirstNetApplication/FirstNetApplication/Program.cs
@@ -49,6 +49,7 @@ app.MapPost("/employees/add-employee", async (HttpContext httpContext) =>
 
 
 //MiddleWare
+/*
 app.Use(async (httpContext, next) =>
 {
     await httpContext.Response.WriteAsync("First MiddleWare \t");
@@ -70,6 +71,66 @@ app.Use(async (HttpContext httpContext, RequestDelegate next) =>
     await httpContext.Response.WriteAsync("Third MiddleWare");
 
     await httpContext.Response.WriteAsync("\n After Mid 3");
+});
+*/
+
+app.Use(async (httpContext, next) =>
+{
+
+    await httpContext.Response.WriteAsync
+    ($"{httpContext.Request.Method} /t {httpContext.Request.Path}");
+    await next(httpContext);
+    await httpContext.Response.WriteAsync("\n request has been successfully");
+});
+
+app.Use(async (httpContext, next) =>
+{
+    if (!httpContext.Request.Headers.ContainsKey("X-Auth-Token"))
+    {
+       httpContext.Response.StatusCode = 400;
+        return;
+    }
+    await next(httpContext);
+    await httpContext.Response.WriteAsync("request was auth");
+});
+
+app.Use(async (httpContext, next) =>
+{
+    if (!httpContext.Request.Headers.ContainsKey("User-Role"))
+    {
+        httpContext.Response.StatusCode = 401;
+        return;
+    }
+    var role = httpContext.Request.Headers.ContainsKey("User-Role").ToString();
+    if (role == "Admin")
+    {
+        httpContext.Response.Headers.Add("X-Admin-Access", "Granted");
+    }
+    else
+    {
+        httpContext.Response.WriteAsync("access is restricted");
+    }
+    await next(httpContext);
+    httpContext.Response.WriteAsync(role == "Admin" ? "request by admin" : "request by user");
+
+});
+
+app.Use(async (httpContext, next) =>
+{
+    await next(httpContext);
+    if (!httpContext.Response.HasStarted)
+    {
+        httpContext.Response.Headers["X-Processed-Time"] = DateTime.UtcNow.ToString("o");
+    }
+    httpContext.Response.WriteAsync("Response has been modified by adding X-Processed-Time header.");
+});
+
+app.Run(async (httpContext) =>
+{
+    httpContext.Response.WriteAsync("Request processing started");
+
+    httpContext.Response.WriteAsync("Request completed successfully");
+
 });
 app.Run();
 
