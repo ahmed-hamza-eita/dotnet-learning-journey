48599bfa6f6fe93ac8548fbc19b78507b7091cbb
aheit <aeita4@yahoo.com>
2026-07-26 16:24:47 +0300

    feat(MapPost)

--- 06-ASP.NET/FirstNetApplication/FirstNetApplication/Program.cs
+++ 06-ASP.NET/FirstNetApplication/FirstNetApplication/Program.cs
@@ -1,6 +1,9 @@
+using System.Text.Json;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+/*
 app.MapGet("/", async (HttpContext httpContext) =>
 {
     if (httpContext.Request.Method == "GETt")
@@ -17,7 +20,7 @@ app.MapGet("/", async (HttpContext httpContext) =>
     string method = httpContext.Request.Method;
     string url = httpContext.Request.Path;
     string? userAgent = null;
-    await httpContext.Response.WriteAsync($"method is {method} and url is {url}");
+    await httpContext.Response.WriteAsync($"method is {method} and url is {url} \n");
 
     //User Agent
     if (httpContext.Request.Headers.ContainsKey("user-agent"))
@@ -27,5 +30,23 @@ app.MapGet("/", async (HttpContext httpContext) =>
     userAgent = userAgent ?? "user agent does not exits";
     await httpContext.Response.WriteAsync($"userAgent is {userAgent}");
 });
+*/
 
+//Post Method
+app.MapPost("/employees/add-employee", async (HttpContext httpContext) =>
+{
+    using (StreamReader stream = new StreamReader(httpContext.Request.Body))
+    {
+        var body = await stream.ReadToEndAsync();
+        var response = JsonSerializer.Deserialize<Employee>(body);
+        await httpContext.Response.WriteAsync($"Name is {response?.name} Age is {response?.age}");
+    }
+
+});
 app.Run();
+
+public class Employee
+{
+    public string name { set; get; }
+    public int age { set; get; }
+}
