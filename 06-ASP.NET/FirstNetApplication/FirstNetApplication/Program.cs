fba2549614f2c9b2e830e00a5cdf90afae781af5
aheit <aeita4@yahoo.com>
2026-07-26 03:54:21 +0300

    feat(response and request headers)

--- 06-ASP.NET/FirstNetApplication/FirstNetApplication/Program.cs
+++ 06-ASP.NET/FirstNetApplication/FirstNetApplication/Program.cs
@@ -8,11 +8,24 @@ app.MapGet("/", async (HttpContext httpContext) =>
         httpContext.Response.StatusCode = 200;
         await httpContext.Response.WriteAsync("My Response");
     }
-    else {
+    else
+    {
         httpContext.Response.StatusCode = 401;
-        await httpContext.Response.WriteAsync("Error");
+        await httpContext.Response.WriteAsync("Error \n");
     }
-    
+
+    string method = httpContext.Request.Method;
+    string url = httpContext.Request.Path;
+    string? userAgent = null;
+    await httpContext.Response.WriteAsync($"method is {method} and url is {url}");
+
+    //User Agent
+    if (httpContext.Request.Headers.ContainsKey("user-agent"))
+    {
+        userAgent = httpContext.Request.Headers["user-agent"];
+    }
+    userAgent = userAgent ?? "user agent does not exits";
+    await httpContext.Response.WriteAsync($"userAgent is {userAgent}");
 });
 
 app.Run();
