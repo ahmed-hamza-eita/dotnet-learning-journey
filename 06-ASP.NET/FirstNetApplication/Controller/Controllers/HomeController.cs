0adbc1979177d50b902bb0b98abd52f54b979b8c
aheit <aeita4@yahoo.com>
2026-08-02 18:11:51 +0300

    feat (Controller): how to configure controller

---
+++ 06-ASP.NET/FirstNetApplication/Controller/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Controller.Controllers
+{
+    public class HomeController
+    {
+        [Route("home/index")]
+        [Route("/")] //default route
+        public string Index()
+        {
+            return "Hello world!";
+        }
+    }
+}
--- 06-ASP.NET/FirstNetApplication/Controller/Program.cs
+++ 06-ASP.NET/FirstNetApplication/Controller/Program.cs
@@ -1,6 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
+
+//register all controller
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+/*
+app.UseRouting();
+app.UseEndpoints(endPoints =>
+{
+    //endPoints("url1,actionMethod); instead write each endpoint use app.MapControllers();
+    app.MapControllers();
+});
+Or USe */
+app.MapControllers();
 
 app.Run();
