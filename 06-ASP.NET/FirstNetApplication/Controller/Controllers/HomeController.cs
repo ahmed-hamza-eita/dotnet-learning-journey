d786979524189da6fe79dd5be193e85b712d22fb
aheit <aeita4@yahoo.com>
2026-08-02 19:48:36 +0300

    feat (Controller): simplify ContentResult

--- 06-ASP.NET/FirstNetApplication/Controller/Controllers/HomeController.cs
+++ 06-ASP.NET/FirstNetApplication/Controller/Controllers/HomeController.cs
@@ -1,26 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-namespace Controller.Controllers
+
+public class HomeController(IHttpContextAccessor httpContextAccessor) : Controller
 {
-    public class HomeController(IHttpContextAccessor httpContextAccessor)
+    [Route("home/index")]
+    [Route("/")] //default route
+    public string Index()
     {
-        [Route("home/index")]
-        [Route("/")] //default route
-        public string Index()
-        {
 
-            return "Hello world!";
-        }
+        return "Hello world!";
+    }
 
-        [Route("home/about/{id:int}")]
-        public ContentResult About()
-        {
-            var id = httpContextAccessor.HttpContext!.Request.RouteValues["id"]?.ToString();
-            return new ContentResult
-            {
-                Content = $"About page for id: {id}",
-                ContentType = "text/html"
-            };
-        }
+    [Route("home/about/{id:int}")]
+    public ContentResult About()
+    {
+        var id = httpContextAccessor.HttpContext!.Request.RouteValues["id"]?.ToString();
+        //return new ContentResult
+        //{
+        //    Content = $"About page for id: {id}",
+        //    ContentType = "text/html"
+        //};
+        return Content($"About page for id: {id}", "text/html");
     }
 }
+
