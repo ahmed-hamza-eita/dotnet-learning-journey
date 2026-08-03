de81bf4326ba87560f4aa2e3d9b059255f9a3dda
aheit <aeita4@yahoo.com>
2026-08-03 06:32:12 +0300

    feat (Controller): handle external files by PhysicalFileResult

--- 06-ASP.NET/FirstNetApplication/Controller/Controllers/FileController.cs
+++ 06-ASP.NET/FirstNetApplication/Controller/Controllers/FileController.cs
@@ -6,7 +6,15 @@ public class FileController : Controller
     [Route("file")]
     public IActionResult GetPdf()
     {
-        return File("PDF/Certificate.pdf","application/pdf");
+        return File("PDF/Certificate.pdf", "application/pdf");
+    }
+
+    [Route("outside-file")]
+    public PhysicalFileResult GetOutsidePdf()
+    {
+        string path = @"C:\Users\aheit\OneDrive\Desktop\Certificate.pdf";
+        string type = "application/pdf";
+        return PhysicalFile(path, type);
     }
 }
 
