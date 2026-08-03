3f79a87c4402a47e4d51c5830b25a8701dd5991e
aheit <aeita4@yahoo.com>
2026-08-03 06:41:34 +0300

    feat (Controller): handle external files by FileContentResult

--- 06-ASP.NET/FirstNetApplication/Controller/Controllers/FileController.cs
+++ 06-ASP.NET/FirstNetApplication/Controller/Controllers/FileController.cs
@@ -16,5 +16,20 @@ public class FileController : Controller
         string type = "application/pdf";
         return PhysicalFile(path, type);
     }
+
+    [Route("byte-file")]
+    public FileContentResult GetFileAsByte()
+    {
+        string path = @"C:\Users\aheit\OneDrive\Desktop\Certificate.pdf";
+        string type = "application/pdf";
+        if (!System.IO.File.Exists(path))
+        {
+            //not found
+        }
+
+        var fileByte = System.IO.File.ReadAllBytes(path);
+
+        return File(fileByte, type);
+    }
 }
 
