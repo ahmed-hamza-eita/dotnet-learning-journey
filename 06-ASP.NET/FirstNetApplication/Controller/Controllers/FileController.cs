802129c69dca1d90736f1b3bda4c8fcf35f6556a
aheit <aeita4@yahoo.com>
2026-08-03 06:20:41 +0300

    feat (Controller): handle files that in wwwroot

---
+++ 06-ASP.NET/FirstNetApplication/Controller/Controllers/FileController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+public class FileController : Controller
+{
+    [Route("file")]
+    public IActionResult GetPdf()
+    {
+        return File("PDF/Certificate.pdf","application/pdf");
+    }
+}
+
