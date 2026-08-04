767dba602383316d6f6eb9290f8e6708396fd253
aheit <aeita4@yahoo.com>
2026-08-04 23:39:08 +0300

    feat (RESTFUL API):add models and create DB by migration

--- 06-ASP.NET/API/API/Data/AppDbContext.cs
+++ 06-ASP.NET/API/API/Data/AppDbContext.cs
@@ -1,11 +1,15 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using API.Data.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { 
-        
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
         }
+
+        public DbSet<Category> categories { set; get; }
     }
 }
---
+++ 06-ASP.NET/API/API/Data/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Data.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { set; get; }
+
+        [MaxLength(50)]
+        public string Name { set; get; }
+
+    }
+}
