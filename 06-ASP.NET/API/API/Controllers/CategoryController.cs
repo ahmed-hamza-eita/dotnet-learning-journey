a5bbf98dbdd0531a87f3cc54ca6a9e88926adea8
aheit <aeita4@yahoo.com>
2026-08-05 00:32:07 +0300

    feat (RESTFUL API): implement controller and test GET method

---
+++ 06-ASP.NET/API/API/Controllers/CategoryController.cs
@@ -0,0 +1,28 @@
+﻿using API.Data;
+using API.Data.Models;
+using API.Repositories.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryRepository _repository;
+        public CategoryController(ICategoryRepository repository)
+        {
+            _repository = repository;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Category>>> GetAllCategories()
+        {
+            var categories = await _repository.GetAllAsync();
+            return Ok(categories);
+        }
+    }
+}
