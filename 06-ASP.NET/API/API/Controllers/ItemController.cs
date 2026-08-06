a906279625afc17db38a1c250b38292d2e446a4a
aheit <aeita4@yahoo.com>
2026-08-06 22:48:48 +0300

    API (Item Section): setup controller

---
+++ 06-ASP.NET/API/API/Controllers/ItemController.cs
@@ -0,0 +1,19 @@
+﻿using API.Data.Models;
+using API.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemController : ControllerBase
+    {
+        private readonly IItemRepository _repository;
+
+        public ItemController(IItemRepository repository)
+        {
+            _repository = repository;
+        }
+
+    }
+}
