5b7a6de5371597ec698de617554c991a7189104b
aheit <aeita4@yahoo.com>
2026-08-06 06:50:17 +0300

    Task_1 :setup generic Repository

---
+++ 06-ASP.NET/API/Task_1/Repositories/Implementation/GenericRepository.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Task_1.Data;
+using Task_1.Repositories.Interfaces;
+
+namespace Task_1.Repositories.Implementation
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly AppDbContext _context;
+        private readonly DbSet<T> _dbSet;
+        public GenericRepository(AppDbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public async Task AddAsync(T entity) => await _dbSet.AddAsync(entity);
+
+
+        public void Delete(T entity) => _dbSet.Remove(entity);
+
+
+        public async Task<IEnumerable<T>> GetAllAsync() => await _dbSet.ToListAsync();
+
+
+        public async Task<T?> GetByIdAsyns(int Id) => await _dbSet.FindAsync(Id);
+
+
+        public async Task<bool> SaveChangesAsync() => await _context.SaveChangesAsync() > 0;
+
+        public void Update(T entity) => _dbSet.Update(entity);
+
+
+    }
+}
---
+++ 06-ASP.NET/API/Task_1/Repositories/Interfaces/IGenericRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace Task_1.Repositories.Interfaces
+{
+    public interface IGenericRepository<T> where T: class 
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T?> GetByIdAsyns(int Id);
+        Task AddAsync(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+        Task<bool> SaveChangesAsync();
+    }
+}
