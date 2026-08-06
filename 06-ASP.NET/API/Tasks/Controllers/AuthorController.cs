26403c247fbc20554feee91db699b97608ed0fd3
aheit <aeita4@yahoo.com>
2026-08-06 18:58:59 +0300

    Task_2: init author controller

---
+++ 06-ASP.NET/API/Tasks/Controllers/AuthorController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Tasks.Repositories.Implementation;
+
+namespace Tasks.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorController : ControllerBase
+    {
+        private readonly AuthorRepository _repository;
+        public AuthorController(AuthorRepository repository)
+        {
+            _repository = repository;
+        }
+    }
+}
