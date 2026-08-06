394aee7672b01e939d2ac64922f0b2c0282abeb0
aheit <aeita4@yahoo.com>
2026-08-06 05:56:35 +0300

    Task_1 : setup db context and connection string

---
+++ 06-ASP.NET/API/Task_1/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Task_1.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
--- 06-ASP.NET/API/Task_1/Program.cs
+++ 06-ASP.NET/API/Task_1/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Task_1.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//register AbbDbContext
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
