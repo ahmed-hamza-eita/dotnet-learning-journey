9b37c0654be517c6c2997217442636de197d669b
aheit <aeita4@yahoo.com>
2026-08-10 00:57:21 +0300

    feat: add seed data for Photo entity via HasData

---
+++ projects/E-commerce/ECommerce.Infrastructure/Data/Config/PhotoConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using ECommerce.Core.Entities.Products;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ECommerce.Infrastructure.Data.Config
+{
+    public class PhotoConfiguration : IEntityTypeConfiguration<Photo>
+    {
+        public void Configure(EntityTypeBuilder<Photo> builder)
+        {
+            builder.Property(n => n.Name).IsRequired();
+
+            builder.HasData(new Photo { Id = 1, Name = "test-photo-1.jpg", ProductId = 1 });
+        }
+    }
+}
---
+++ projects/E-commerce/ECommerce.Infrastructure/Data/Migrations/20260809215523_SeedPhotosData.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ECommerce.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedPhotosData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Photos",
+                columns: new[] { "Id", "Name", "ProductId" },
+                values: new object[] { 1, "test-photo-1.jpg", 1 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Photos",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
