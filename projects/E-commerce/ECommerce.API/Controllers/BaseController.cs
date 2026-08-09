3c7ed630b59efb61cb44ffb29b7b289f98e7c707
aheit <aeita4@yahoo.com>
2026-08-09 03:22:46 +0300

    feat: add BaseController and CategoriesController with GetAll endpoint

---
+++ projects/E-commerce/ECommerce.API/Controllers/BaseController.cs
@@ -0,0 +1,16 @@
+﻿using ECommerce.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.API.Controllers
+{
+
+    [ApiController]
+    public abstract class BaseController : ControllerBase
+    {
+        protected readonly IUnitOfWork _unitOfWork;
+        public BaseController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+    }
+}
---
+++ projects/E-commerce/ECommerce.API/Controllers/CategoriesController.cs
@@ -0,0 +1,31 @@
+﻿using ECommerce.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace ECommerce.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class CategoriesController : BaseController
+    {
+        public CategoriesController(IUnitOfWork unitOfWork) : base(unitOfWork)
+        {
+        }
+
+        [HttpGet("get-all")]
+        public async Task<ActionResult> GetAllCategories()
+        {
+            try
+            {
+                var categories = await _unitOfWork.CategoryRepository.GetAllAsync();
+                if (!categories.Any())
+                    return NotFound();
+                return Ok(categories);
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
