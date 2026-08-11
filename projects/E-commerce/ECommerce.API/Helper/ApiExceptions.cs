82c9d1017872fbb4629364c22f006c0fdab7ca0a
aheit <aeita4@yahoo.com>
2026-08-11 15:34:27 +0300

    feat: add ErrorController and BugController for testing error responses

--- projects/E-commerce/ECommerce.API/Helper/ApiExceptions.cs
+++ projects/E-commerce/ECommerce.API/Helper/ApiExceptions.cs
@@ -2,8 +2,8 @@
 {
     public class ApiExceptions : ResponseAPI
     {
-        public string Details { set; get; }
-        public ApiExceptions(int statusCode, string? message = null, string details = null) : base(statusCode, message)
+        public string? Details { set; get; }
+        public ApiExceptions(int statusCode, string? message = null, string? details = null) : base(statusCode, message)
         {
             Details = details;
         }
