d470fde1911b8871fb4bb52edf113b9a0c1a7567
aheit <aeita4@yahoo.com>
2026-08-11 16:08:56 +0300

    feat: handle security(XSS CSRF)

--- projects/E-commerce/ECommerce.API/Middleware/ExceptionsMiddleware.cs
+++ projects/E-commerce/ECommerce.API/Middleware/ExceptionsMiddleware.cs
@@ -24,6 +24,7 @@ namespace ECommerce.API.Middleware
         {
             try
             {
+                ApplySecurity(context);
                 if (IsRequestAllowed(context) == false)
                 {
                     context.Response.StatusCode = (int)HttpStatusCode.TooManyRequests;
@@ -76,6 +77,13 @@ namespace ECommerce.API.Middleware
                 _memoryCache.Set(cachKey, (timeSpan: dateNow, count: 1), _rateLimitWindow);
             }
             return true;
+        }
+
+        private void ApplySecurity(HttpContext context)
+        {
+            context.Response.Headers["X-Content-Type-Options"] = "nosniff";
+            context.Response.Headers["X-XSS-Protection"] = "1;mode=block";
+            context.Response.Headers["X-Frame-Options"] = "DENY";
         }
     }
 
