c9e25633e5fc046a0d832af760146dea0f9cf1bc
aheit <aeita4@yahoo.com>
2026-08-18 06:55:19 +0300

    feat(ui): add navbar component

---
+++ projects/client/src/app/core/core-module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavBar } from './nav-bar/nav-bar';
+
+@NgModule({
+  declarations: [NavBar],
+  imports: [CommonModule],
+  exports:[NavBar]
+})
+export class CoreModule {}
